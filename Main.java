9cc908ea989e5ef2972dee75fcd2aa610c86d886
dagov42 <62306927+dagov42@users.noreply.github.com>
2020-07-17 15:32:35 +0300

    new hometask

--- Main.java
+++ Main.java
@@ -1,33 +1,43 @@
-package geekbrains.HomeTask_6;
+package geekbrains.HomeTask_7;
 
-import java.util.ArrayList;
-import java.util.Random;
+import geekbrains.HomeTask_3.Stack;
 
 public class Main {
-    final static int TREES = 20;
-    static int max = 100;
-    static int min = -100;
-    static int LEVEL = 6;
-
-
     public static void main(String[] args) {
-        int balancedCounter = 0;
-        for (int i = 0; i < TREES; i++) {
-            ArrayList<Integer> list = new ArrayList<>();
-            Randoms(list, 2000);
-            Tree tree = new Tree(list);
-            balancedCounter += tree.isBalanced();
-        }
-        System.out.println(balancedCounter * 100f / TREES + "%");
-    }
+        Graph graph = new Graph();
+        graph.addVertex('a');
+        graph.addVertex('b');
+        graph.addVertex('c');
+        graph.addVertex('d');
+        graph.addVertex('e');
+        graph.addVertex('f');
+        graph.addVertex('g');
+        graph.addVertex('h');
+        graph.addVertex('j');
+        graph.addVertex('k');
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(4, 1);
+        graph.addEdge(2, 5);
+        graph.addEdge(3, 7);
+        graph.addEdge(6, 8);
+        graph.addEdge(7, 9);
+        graph.addEdge(6, 5);
+        graph.addEdge(6, 9);
+        graph.addEdge(8, 9);
+        graph.addEdge(6, 8);
+        graph.addEdge(4, 8);
+        graph.addEdge(9, 7);
+        graph.addEdge(8, 6);
 
-    public static void Randoms(ArrayList<Integer> list, int quantity) {
-        Random random = new Random();
-        while (list.size() < quantity) {
-            max -= min;
-            int number = (int) (Math.random() * ++max) + min;
-            if (!list.contains(number)) {
-                list.add(number);
+
+        Stack shortestWay = graph.shortestWay('a', 'f');
+        if (shortestWay != null){
+            while (!shortestWay.isEmpty()){
+                System.out.print((char) shortestWay.pop());
+                System.out.print(shortestWay.isEmpty() ? "" : " goto ");
             }
         }
     }
